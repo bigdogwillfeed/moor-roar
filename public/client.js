1c56fe6177eb7e3044a99b0926f6716683fbb8cb
Glitch (moor-roar) <none>
2018-02-25 05:18:03 +0000

    🚃💔 Added remix hierarchy info

--- public/client.js
+++ public/client.js
@@ -19,28 +19,70 @@ $(function() {
     $('#channel').prop('disabled', false).removeClass('diabled');
   }
   
+  var sDomain;
   function setDomain(domain) {
+    sDomain = domain;
     $('iframe').attr('src', 'https://' + domain + '.glitch.me/');
     $('#title').text(domain);
     noSpin();
+    return domain
+  }
+  
+  var fGetInfo = false;
+  function toggleInfo() {
+    fGetInfo = !fGetInfo
+    setInfo(sDomain)
   }
   
   function nextDomain() {
     spin();
-    $.get('/domain').then(function(data) {
-      setDomain(data);
-      }).fail(function(error) {
+    $.get('/domain')
+      .then(setDomain).then(setInfo)
+      .fail(function(error) {
         console.log(error.statusCode);
-        });
+      });
+  }
+  
+  function setInfo(domain) {
+    $('#info-pane').html('')
+    if (!fGetInfo) return
+    getInfo(domain)
+  }
+  
+  function getInfo(p) {
+    $.get('https://api.glitch.com/projects/' + p)
+      .then(data => {
+        if (data) {
+          addInfo(data.domain)
+          if (data.baseId) {
+            // keep grabbing remix bases
+            return getInfo(data.baseId)
+          }
+          // linkify the remix outline so we can surf it, too!
+          $('#info-pane li').on('click', function() {
+            spin();
+            setDomain(this.textContent)
+          })
+        }
+      })
+      .fail(function(error) {
+        console.log(error)
+      })
+  }
+  
+  function addInfo(domain) {
+    let outline = $('#info-pane').html()
+    $('#info-pane').html('<ul><li>' + domain + '</li>' + outline + '</ul>')
   }
   
   $('iframe').on('load', function() {
     $('#static').hide();
   });
   
-  $('#channel').click(nextDomain);
+  $('#channel').click(nextDomain)
+  $('#info').click(toggleInfo)
   $('#title').on('click', function() {
-    window.open('https://glitch.com/~' + this.textContent);
-  });
-  nextDomain();
-});
+    window.open('https://glitch.com/~' + this.textContent)
+  })
+  nextDomain()
+})
